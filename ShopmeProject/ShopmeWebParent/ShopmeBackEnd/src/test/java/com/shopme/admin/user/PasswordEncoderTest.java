8b2d836a2124154a5c025cdfa1aec94b87de6b10
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-16 02:29:03 +0530

    Added a simple test that verifies that a password is encrypted successfully

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/PasswordEncoderTest.java
@@ -0,0 +1,17 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTest {
+	@Test
+	public void testEncodePassword() {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String rawPassword = "ankitjoshi3011";
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+		
+		assertThat(passwordEncoder.matches(rawPassword, encodedPassword)).isTrue();
+	}
+}
