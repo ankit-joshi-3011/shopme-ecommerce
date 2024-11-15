96a12a0b75f1b8a921315e14fbea9db55fa7643a
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-15 23:47:42 +0530

    Autowired the UserRepository correctly

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -10,22 +10,17 @@ import com.shopme.common.entity.User;
 
 @Service
 public class UserService {
+	@Autowired
 	private UserRepository repository;
 
-	@Autowired
-	public UserService(UserRepository repository) {
-		super();
-		this.repository = repository;
-	}
-	
 	public List<User> listAll() {
 		Iterable<User> users = repository.findAll();
 		List<User> returnedUsers = new ArrayList<>();
-		
+
 		for (User user : users) {
 			returnedUsers.add(user);
 		}
-		
+
 		return returnedUsers;
 	}
 }
