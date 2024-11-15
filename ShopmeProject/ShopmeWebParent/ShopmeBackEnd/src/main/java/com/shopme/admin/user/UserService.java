5d00d6205f257937d56723f71dd0f6ce9b5a937b
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-16 02:42:35 +0530

    Ensured that passwords are encrypted before being stored in the database

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Role;
@@ -16,6 +17,9 @@ public class UserService {
 
 	@Autowired
 	private RoleRepository roleRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public List<User> listAll() {
 		Iterable<User> users = userRepository.findAll();
@@ -40,6 +44,12 @@ public class UserService {
 	}
 
 	public void save(User user) {
+		encodePassword(user);
 		userRepository.save(user);
+	}
+	
+	private void encodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 	}
 }
