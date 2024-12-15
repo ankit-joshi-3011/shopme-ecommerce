397c0e88342473bd525c783276ba26a7e7508fb9
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-15 19:05:36 +0530

    Added logic to save the user's photo name to the database if it exists

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,10 +1,12 @@
 package com.shopme.admin.user;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.FileUploadUtility;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
@@ -46,9 +49,24 @@ public class UserController {
 	// redirected. The redirect attributes will then be available on the target page
 	// where it redirects to.
 	@PostMapping("/users/save")
-	public String saveUser(User user, RedirectAttributes attributes,
-			@RequestParam("image") MultipartFile multipartFile) {
-		service.save(user);
+	public String saveUser(User user, RedirectAttributes attributes, @RequestParam("image") MultipartFile multipartFile)
+			throws IOException {
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			user.setPhotos(fileName);
+			User savedUser = service.save(user);
+
+			String uploadDirectory = "user-photos/" + savedUser.getId();
+
+			FileUploadUtility.cleanDirectory(uploadDirectory);
+			FileUploadUtility.saveFile(uploadDirectory, fileName, multipartFile);
+		} else {
+			if (user.getPhotos() == null || user.getPhotos().isEmpty()) {
+				user.setPhotos(null);
+			}
+
+			service.save(user);
+		}
 
 		attributes.addFlashAttribute("message", "The user has been saved successfully.");
 
