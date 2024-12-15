cfeee752f05d69fd4b1617d935b1f0c9dc93915b
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-15 18:55:36 +0530

    Added a method to clean the user photo directory before saving a new photo.

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtility.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtility.java
@@ -25,4 +25,24 @@ public class FileUploadUtility {
 			throw new IOException("Could not save file: " + fileName, ex);
 		}
 	}
+
+	public static void cleanDirectory(String directory) {
+		Path directoryPath = Paths.get(directory);
+
+		try {
+			Files.list(directoryPath).forEach(file -> {
+				if (!Files.isDirectory(file)) {
+					try {
+						Files.delete(file);
+					} catch (IOException e) {
+						System.err.println("Error in deleting file: " + file);
+						e.printStackTrace();
+					}
+				}
+			});
+		} catch (IOException e) {
+			System.err.println("Error in listing directory: " + directory);
+			e.printStackTrace();
+		}
+	}
 }
