48b3a637cfa3af0179417b0c2e318fffd425e3f3
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-15 18:23:58 +0530

    Added the MvcConfigurer class to expose user photos to the web application

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfigurer.java
@@ -0,0 +1,22 @@
+package com.shopme.admin;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfigurer implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		String userPhotosDirectoryName = "user-photos";
+
+		Path userPhotosDirectory = Paths.get(userPhotosDirectoryName);
+		String userPhotosPath = userPhotosDirectory.toFile().getAbsolutePath();
+
+		registry.addResourceHandler("/" + userPhotosDirectoryName + "/**")
+				.addResourceLocations("file:/" + userPhotosPath + "/");
+	}
+
+}
