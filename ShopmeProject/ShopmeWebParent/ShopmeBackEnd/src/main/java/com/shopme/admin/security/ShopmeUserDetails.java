3f73dc5846f00a5460a0cf82a0eea9f777e9e9c1
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-17 02:33:12 +0530

    Added code to show the logged-in user's full name in the title bar

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
@@ -47,4 +47,8 @@ public class ShopmeUserDetails implements UserDetails {
 	public boolean isEnabled() {
 		return user.isEnabled();
 	}
+
+	public String getFullName() {
+		return user.getFirstName() + " " + user.getLastName();
+	}
 }
