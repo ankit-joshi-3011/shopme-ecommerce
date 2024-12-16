8e51a58797724480fb91b7d42be81d3c5bc9ddc4
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-16 23:19:56 +0530

    Auto-formatted ShopmeFrontEndApplication

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/ShopmeFrontEndApplication.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/ShopmeFrontEndApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShopmeFrontEndApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShopmeFrontEndApplication.class, args);
 	}
-
 }
