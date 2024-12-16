8363444b6b4ec277ecb31a164f302932bdc20ecb
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-16 23:18:25 +0530

    Auto-formatted RoleRepositoryTests

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
@@ -21,26 +21,27 @@ import com.shopme.common.entity.Role;
 @Rollback(false)
 public class RoleRepositoryTests {
 	private RoleRepository repository;
-	
+
 	@Autowired
 	public RoleRepositoryTests(RoleRepository repository) {
 		this.repository = repository;
 	}
-	
+
 	@Test
 	public void testCreateAdminRole() {
 		Role roleAdmin = new Role("Admin", "Manage everything");
 		Role savedRole = repository.save(roleAdmin);
 		assertThat(savedRole.getId()).isGreaterThan(0);
 	}
-	
+
 	@Test
 	public void testCreateRemainingRoles() {
-		Role roleSalesperson = new Role("Salesperson", "Manage product price, customers, shipping, orders and sales report");
+		Role roleSalesperson = new Role("Salesperson",
+				"Manage product price, customers, shipping, orders and sales report");
 		Role roleEditor = new Role("Editor", "Manage categories, brands, products, articles and menus");
 		Role roleShipper = new Role("Shipper", "View products, view orders and update order status");
 		Role roleAssistant = new Role("Assistant", "Manage questions and reviews");
-		
+
 		repository.saveAll(List.of(roleSalesperson, roleEditor, roleShipper, roleAssistant));
 	}
 }
