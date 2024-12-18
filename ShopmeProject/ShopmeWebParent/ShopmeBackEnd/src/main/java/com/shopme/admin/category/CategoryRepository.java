963796c80742c3a27eab9c8cc3941d0d0ed42595
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-18 23:56:54 +0530

    Added the CategoryRepository

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.shopme.admin.category;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.shopme.common.entity.Category;
+
+public interface CategoryRepository
+		extends CrudRepository<Category, Integer>, PagingAndSortingRepository<Category, Integer> {
+
+}
