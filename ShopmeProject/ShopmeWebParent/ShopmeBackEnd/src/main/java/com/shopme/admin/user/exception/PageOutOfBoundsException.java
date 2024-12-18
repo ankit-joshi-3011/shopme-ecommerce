1925af4e1b80ee8499fce343af3f5c1a1846db05
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-18 23:20:39 +0530

    Created a new exception which will be thrown when the requested page is out of bounds

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/exception/PageOutOfBoundsException.java
@@ -0,0 +1,12 @@
+package com.shopme.admin.user.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The page you requested is out of bounds")
+public class PageOutOfBoundsException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3783725811438398319L;
+}
