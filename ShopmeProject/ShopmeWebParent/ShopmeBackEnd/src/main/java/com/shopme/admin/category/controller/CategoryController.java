4a6943f190ef4a4e7e286cfa5afe8b06d82df489
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-21 21:03:58 +0530

    Added the saveCategory method to the CategoryController

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/controller/CategoryController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/controller/CategoryController.java
@@ -1,11 +1,19 @@
 package com.shopme.admin.category.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.shopme.admin.FileUploadUtility;
 import com.shopme.admin.category.CategoryService;
 import com.shopme.common.entity.Category;
 
@@ -33,5 +41,22 @@ public class CategoryController {
 		model.addAttribute("listCategories", listCategories);
 		model.addAttribute("pageTitle", "Create New Category");
 		return "categories/category_form";
+	}
+
+	@PostMapping("/categories/save")
+	public String saveCategory(Category category, RedirectAttributes attributes, @RequestParam("fileImage") MultipartFile multipartFile) throws IOException {
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		category.setImage(fileName);
+
+		Category savedCategory = service.save(category);
+
+		String uploadDirectory = "../category-images/" + savedCategory.getId();
+
+		FileUploadUtility.cleanDirectory(uploadDirectory);
+		FileUploadUtility.saveFile(uploadDirectory, fileName, multipartFile);
+
+		attributes.addFlashAttribute("message", "The category has been saved successfully.");
+
+		return "redirect:/categories";
 	}
 }
