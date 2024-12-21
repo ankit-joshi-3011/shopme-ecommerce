7458cf7190b145f39c14069f797ce60e3231c42f
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-21 19:19:32 +0530

    Annotated the CategoryService with Service

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.shopme.common.entity.Category;
 
+@Service
 public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
