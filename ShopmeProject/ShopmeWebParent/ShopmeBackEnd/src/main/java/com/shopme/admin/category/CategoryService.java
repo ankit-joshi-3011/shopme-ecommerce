46432afcb170f01b218fb634474b6ecb92471c42
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-21 20:24:13 +0530

    Added the logic to list the categories hierarchically

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -2,6 +2,7 @@ package com.shopme.admin.category;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,39 @@ public class CategoryService {
 		}
 
 		return returnedCategories;
+	}
+
+	public List<Category> listCategoriesInForm() {
+		Iterable<Category> categories = categoryRepository.findAll();
+		List<Category> returnedCategories = new ArrayList<>();
+
+		for (Category category : categories) {
+			if (category.getParent() == null) {
+				returnedCategories.add(category);
+
+				listSubCategories(category, 2, returnedCategories);
+			}
+		}
+
+		return returnedCategories;
+	}
+
+	private void listSubCategories(Category parent, int numberOfHyphens, List<Category> returnedCategories) {
+		StringBuilder hyphens = new StringBuilder();
+
+		for (int i = 0; i < numberOfHyphens; i++) {
+			hyphens.append("-");
+		}
+
+		Set<Category> children = parent.getChildren();
+
+		for (Category subCategory : children) {
+			subCategory.setName(hyphens.toString() + subCategory.getName());
+			returnedCategories.add(subCategory);
+
+			if (!subCategory.getChildren().isEmpty()) {
+				listSubCategories(subCategory, numberOfHyphens + 2, returnedCategories);
+			}
+		}
 	}
 }
