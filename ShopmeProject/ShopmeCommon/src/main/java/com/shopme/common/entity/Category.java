ce49761f155016b7b43039547111ad180e9fb940
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-21 22:16:08 +0530

    Added logic to display the image on the category page

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -12,6 +12,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "categories")
@@ -124,5 +125,10 @@ public class Category {
 
 	public void setChildren(Set<Category> children) {
 		this.children = children;
+	}
+
+	@Transient
+	public String getImagePath() {
+		return "/category-images/" + id + "/" + image;
 	}
 }
