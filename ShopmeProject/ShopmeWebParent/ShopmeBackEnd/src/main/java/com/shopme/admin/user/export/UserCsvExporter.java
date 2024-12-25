f6542110e148a23c3567bdfa14e2bbebb8de2b61
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-25 23:01:13 +0530

    Used the AbstractCsvExporter in UserCsvExporter

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/export/UserCsvExporter.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/export/UserCsvExporter.java
@@ -3,20 +3,17 @@ package com.shopme.admin.user.export;
 import java.io.IOException;
 import java.util.List;
 
-import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
-import org.supercsv.prefs.CsvPreference;
-
-import com.shopme.admin.export.AbstractExporter;
+import com.shopme.admin.export.AbstractCsvExporter;
 import com.shopme.common.entity.User;
 
 import jakarta.servlet.http.HttpServletResponse;
 
-public class UserCsvExporter extends AbstractExporter {
+public class UserCsvExporter extends AbstractCsvExporter {
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
-		super.setResponseHeader(response, "users", "text/csv", ".csv");
+		this.setResponseHeader(response, "users");
 
-		ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+		ICsvBeanWriter csvWriter = this.getWriter(response);
 
 		String[] csvHeader = { "User ID", "E-mail", "First Name", "Last Name", "Roles", "Enabled" };
 		String[] fieldMapping = { "id", "email", "firstName", "lastName", "roles", "enabled" };
