a014c6f16167fdc8c40827599ace4f23f5da6d25
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-25 22:29:00 +0530

    Made the AbstractExporter class abstract

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/export/AbstractExporter.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/export/AbstractExporter.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class AbstractExporter {
+public abstract class AbstractExporter {
 	public void setResponseHeader(HttpServletResponse response, String filePrefix, String contentType, String extension) {
 		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
 		String timestamp = dateFormatter.format(new Date());
