a34cfe54bf55ed6f370e9c2956bb63057d08acb5
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-25 23:06:14 +0530

    Added the CategoryCsvExporter to export category details to a CSV file

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/export/CategoryCsvExporter.java
@@ -0,0 +1,28 @@
+package com.shopme.admin.category.export;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.supercsv.io.ICsvBeanWriter;
+import com.shopme.admin.export.AbstractCsvExporter;
+import com.shopme.common.entity.Category;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CategoryCsvExporter extends AbstractCsvExporter {
+	public void export(List<Category> listCategory, HttpServletResponse response) throws IOException {
+		this.setResponseHeader(response, "categories");
+
+		ICsvBeanWriter csvWriter = this.getWriter(response);
+
+		String[] csvHeader = { "Category ID", "Category Name" };
+		String[] fieldMapping = { "id", "name" };
+
+		csvWriter.writeHeader(csvHeader);
+
+		for (Category category : listCategory) {
+			csvWriter.write(category, fieldMapping);
+		}
+
+		csvWriter.close();
+	}
+}
