b44e026b582cda5c8656ee51d5eaaa60554c36d6
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-29 23:06:50 +0530

    Added a method to handle the /brands/new URL in the BrandController

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/controller/BrandController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/controller/BrandController.java
@@ -6,22 +6,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.shopme.admin.brand.BrandService;
+import com.shopme.admin.category.CategoryService;
 import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 
 @Controller
 public class BrandController {
-	private BrandService service;
+	private BrandService brandService;
+	private CategoryService categoryService;
 
-	public BrandController(BrandService service) {
-		this.service = service;
+	public BrandController(BrandService brandService, CategoryService categoryService) {
+		this.brandService = brandService;
+		this.categoryService = categoryService;
 	}
 
 	@GetMapping("/brands")
 	public String listAllBrands(Model model) {
-		List<Brand> listBrands = service.listAllBrands();
+		List<Brand> listBrands = brandService.listAllBrands();
 
 		model.addAttribute("listBrands", listBrands);
 
 		return "brands/brands";
 	}
+
+	@GetMapping("/brands/new")
+	public String newBrand(Model model) {
+		Brand brand = new Brand();
+
+		List<Category> listCategories = categoryService.listCategoriesInForm();
+
+		model.addAttribute("brand", brand);
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("pageTitle", "Create New Brand");
+
+		return "brands/brand_form";
+	}
+
 }
