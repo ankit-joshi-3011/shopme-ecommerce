df6525e33718c248faeee91904b60f5711fab636
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-29 22:28:13 +0530

    Added a method to the repository to find a brand by its name

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import com.shopme.common.entity.Brand;
 
 public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer>, CrudRepository<Brand, Integer> {
-
+	public Brand findByName(String name);
 }
