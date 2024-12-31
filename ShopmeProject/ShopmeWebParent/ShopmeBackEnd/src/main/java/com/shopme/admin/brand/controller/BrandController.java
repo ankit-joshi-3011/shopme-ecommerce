21483bea655d40763f20029b6d6b9e5974b242c6
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-31 22:07:46 +0530

    Added the saveBrand() method to the BrandController

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/controller/BrandController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/controller/BrandController.java
@@ -1,10 +1,18 @@
 package com.shopme.admin.brand.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.shopme.admin.FileUploadUtility;
 import com.shopme.admin.brand.BrandService;
 import com.shopme.admin.category.CategoryService;
 import com.shopme.common.entity.Brand;
@@ -42,4 +50,24 @@ public class BrandController {
 		return "brands/brand_form";
 	}
 
+	@PostMapping("/brands/save")
+	public String saveBrand(Brand brand, RedirectAttributes attributes, @RequestParam("fileImage") MultipartFile multipartFile) throws IOException {
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			brand.setLogo(fileName);
+
+			Brand savedBrand = brandService.save(brand);
+
+			String uploadDirectory = "../brand-logo-images/" + savedBrand.getId();
+
+			FileUploadUtility.cleanDirectory(uploadDirectory);
+			FileUploadUtility.saveFile(uploadDirectory, fileName, multipartFile);
+		} else {
+			brandService.save(brand);
+		}
+
+		attributes.addFlashAttribute("message", "The brand has been saved successfully.");
+
+		return "redirect:/brands";
+	}
 }
