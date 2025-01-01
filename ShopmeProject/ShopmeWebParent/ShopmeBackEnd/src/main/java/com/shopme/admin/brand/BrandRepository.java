8381452b2ce248b5edf12502e8c80d5f90cb2f84
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-01 21:42:17 +0530

    Added the findAll() method to the BrandRepository to search by a keyword

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -1,5 +1,8 @@
 package com.shopme.admin.brand;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -9,4 +12,7 @@ public interface BrandRepository extends PagingAndSortingRepository<Brand, Integ
 	public Brand findByName(String name);
 
 	public Long countById(Integer id);
+
+	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+	public Page<Brand> findAll(String keyword, Pageable pageable);
 }
