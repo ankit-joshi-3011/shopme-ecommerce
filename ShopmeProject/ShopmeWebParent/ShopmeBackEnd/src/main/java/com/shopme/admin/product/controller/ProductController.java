31cefb1751eefba5c7c987082ab7538ea59db2a7
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-02 20:52:25 +0530

    Added the ProductController class

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.shopme.admin.product.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.admin.product.ProductService;
+import com.shopme.common.entity.Product;
+
+@Controller
+public class ProductController {
+	private ProductService service;
+
+	public ProductController(ProductService service) {
+		this.service = service;
+	}
+
+	@GetMapping("/products")
+	public String listAll(Model model) {
+		List<Product> listProducts = service.listAll();
+
+		model.addAttribute("listProducts", listProducts);
+
+		return "products/products";
+	}
+}
