59efd9083517200c343eee80c60322e24f642f59
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-02 23:15:06 +0530

    Created a new exception that would be thrown by the BrandRestController in case a brand was not found

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/exception/BrandNotFoundExceptionRestApi.java
@@ -0,0 +1,9 @@
+package com.shopme.admin.brand.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@SuppressWarnings("serial")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Brand not found")
+public class BrandNotFoundExceptionRestApi extends Exception {
+}
