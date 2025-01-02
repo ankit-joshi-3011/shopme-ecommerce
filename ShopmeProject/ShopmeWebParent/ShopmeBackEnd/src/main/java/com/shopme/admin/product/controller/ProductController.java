97da7b7dfa7822ad377d86c95f45bc385866586f
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-02 21:52:40 +0530

    Updated the handler that allows creating a new product to pass the correct details to the view

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/controller/ProductController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/controller/ProductController.java
@@ -6,15 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.shopme.admin.brand.BrandService;
 import com.shopme.admin.product.ProductService;
+import com.shopme.common.entity.Brand;
 import com.shopme.common.entity.Product;
 
 @Controller
 public class ProductController {
 	private ProductService productService;
+	private BrandService brandService;
 
-	public ProductController(ProductService productService) {
+	public ProductController(ProductService productService, BrandService brandService) {
 		this.productService = productService;
+		this.brandService = brandService;
 	}
 
 	@GetMapping("/products")
@@ -28,6 +32,16 @@ public class ProductController {
 
 	@GetMapping("/products/new")
 	public String newProduct(Model model) {
+		Product product = new Product();
+		product.setEnabled(true);
+		product.setInStock(true);
+
+		List<Brand> listBrands = brandService.getBrandsWithIdAndNameInformation();
+
+		model.addAttribute("product", product);
+		model.addAttribute("listBrands", listBrands);
+		model.addAttribute("pageTitle", "Create New Product");
+
 		return "products/product_form";
 	}
 }
