4e098e520df5e26ae7125169ba2f4a49d8205d93
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-02 18:39:14 +0530

    Removed the serialVersionUID from the PageOutOfBoundsException class

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/exception/PageOutOfBoundsException.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/exception/PageOutOfBoundsException.java
@@ -3,10 +3,7 @@ package com.shopme.admin.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@SuppressWarnings("serial")
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The page you requested is out of bounds")
 public class PageOutOfBoundsException extends RuntimeException {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 3783725811438398319L;
 }
