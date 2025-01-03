8bc2ed1e41ef492c2b0ac989c9062b8458e3fd43
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-03 21:02:42 +0530

    Added logic to save the main image of the product

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/controller/ProductController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/controller/ProductController.java
@@ -1,14 +1,19 @@
 package com.shopme.admin.product.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.FileUploadUtility;
 import com.shopme.admin.brand.BrandService;
 import com.shopme.admin.product.ProductService;
 import com.shopme.admin.product.exception.ProductNotFoundException;
@@ -50,8 +55,20 @@ public class ProductController {
 	}
 
 	@PostMapping("/products/save")
-	public String saveProduct(Product product, RedirectAttributes attributes) {
-		productService.save(product);
+	public String saveProduct(Product product, RedirectAttributes attributes, @RequestParam("fileImage") MultipartFile multipartFile) throws IOException {
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			product.setMainImage(fileName);
+
+			Product savedProduct = productService.save(product);
+
+			String uploadDirectory = "../product-images/" + savedProduct.getId();
+
+			FileUploadUtility.cleanDirectory(uploadDirectory);
+			FileUploadUtility.saveFile(uploadDirectory, fileName, multipartFile);
+		} else {
+			productService.save(product);
+		}
 
 		attributes.addFlashAttribute("message", "The product has been saved successfully.");
 
