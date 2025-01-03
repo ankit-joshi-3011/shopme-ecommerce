8fc30509e49317f4dc3dddd8c11d05fb0325c843
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-03 18:26:53 +0530

    Added the handler that saves the product to the database

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/controller/ProductController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/controller/ProductController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.brand.BrandService;
 import com.shopme.admin.product.ProductService;
@@ -43,5 +45,14 @@ public class ProductController {
 		model.addAttribute("pageTitle", "Create New Product");
 
 		return "products/product_form";
+	}
+
+	@PostMapping("/products/save")
+	public String saveProduct(Product product, RedirectAttributes attributes) {
+		productService.save(product);
+
+		attributes.addFlashAttribute("message", "The product has been saved successfully.");
+
+		return "redirect:/products";
 	}
 }
