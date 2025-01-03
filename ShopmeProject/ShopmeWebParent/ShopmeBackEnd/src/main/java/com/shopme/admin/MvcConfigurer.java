cdf9aa5c35197a6282af7034b9aeb3cb1328355a
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-03 21:05:28 +0530

    Added the resource handler for the product-images directory

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfigurer.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfigurer.java
@@ -11,8 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfigurer implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String[] directoryNames = new String[] { "user-photos", "category-images", "brand-logo-images" };
-		boolean[] parentDirectoryOrCurrentDirectory = new boolean[] { false, true, true };
+		String[] directoryNames = new String[] { "user-photos", "category-images", "brand-logo-images", "product-images" };
+		boolean[] parentDirectoryOrCurrentDirectory = new boolean[] { false, true, true, true };
 
 		for (int i = 0; i < directoryNames.length; i++) {
 			Path directoryPath = Paths.get((parentDirectoryOrCurrentDirectory[i] ? "../" : "") + directoryNames[i]);
