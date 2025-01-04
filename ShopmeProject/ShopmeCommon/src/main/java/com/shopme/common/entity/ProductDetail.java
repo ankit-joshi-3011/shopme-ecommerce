fc131505508eef66e7934125c285dcf0cd036df7
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-04 22:26:57 +0530

    Added the ProductDetail entity

---
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/ProductDetail.java
@@ -0,0 +1,60 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product_details")
+public class ProductDetail {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String value;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
