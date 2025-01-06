dfc431089a20bc7b1322f6b631b7246e8447d869
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-06 20:44:53 +0530

    Added a utility method to delete unneeded files

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtility.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtility.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Set;
 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
@@ -58,6 +59,28 @@ public class FileUploadUtility {
 			Files.delete(Paths.get(directory));
 		} catch (IOException e) {
 			System.err.println("Error in deleting directory: " + directory);
+			e.printStackTrace();
+		}
+	}
+
+	public static void deleteUnneededFiles(String directory, Set<String> namesOfFilesToKeep) {
+		Path directoryPath = Paths.get(directory);
+
+		try {
+			Files.list(directoryPath).forEach(file -> {
+				String fileName = file.toFile().getName();
+
+				if (!namesOfFilesToKeep.contains(fileName)) {
+					try {
+						Files.delete(file);
+					} catch (IOException e) {
+						System.err.println("Error in deleting file: " + file);
+						e.printStackTrace();
+					}
+				}
+			});
+		} catch (IOException e) {
+			System.err.println("Error in listing directory: " + directory);
 			e.printStackTrace();
 		}
 	}
