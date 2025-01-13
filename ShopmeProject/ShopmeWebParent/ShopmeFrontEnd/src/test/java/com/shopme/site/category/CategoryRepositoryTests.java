d0f3de345a3264e6a6b1f9cefa63139ba42448fd
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-13 21:29:47 +0530

    Added a test class for the CategoryRepository

---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/site/category/CategoryRepositoryTests.java
@@ -0,0 +1,33 @@
+package com.shopme.site.category;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Category;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class CategoryRepositoryTests {
+	private CategoryRepository repository;
+
+	@Autowired
+	public CategoryRepositoryTests(CategoryRepository repository) {
+		this.repository = repository;
+	}
+
+	@Test
+	public void testListEnabledCategories() {
+		List<Category> enabledCategories = repository.findAllEnabled();
+
+		enabledCategories.forEach(category -> {
+			System.out.println(category.getName() + " (" + category.isEnabled() + ")");
+		});
+	}
+}
