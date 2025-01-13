e33f693f032ee90998035cb8996ffa33fe8e6a77
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-13 21:43:31 +0530

    Added the enabled categories which have no children to the Model so that it can be viewed on the index page

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/MainController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/MainController.java
@@ -1,12 +1,28 @@
 package com.shopme.site;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.common.entity.Category;
+import com.shopme.site.category.CategoryService;
 
 @Controller
 public class MainController {
+	private CategoryService categoryService;
+
+	public MainController(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
+
 	@GetMapping("")
-	public String viewHomePage() {
+	public String viewHomePage(Model model) {
+		List<Category> enabledCategoriesWithNoChildren = categoryService.listEnabledCategoriesWithNoChildren();
+
+		model.addAttribute("listCategories", enabledCategoriesWithNoChildren);
+
 		return "index";
 	}
 }
