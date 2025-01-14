789ca338c4842b94d3a0cad4765d884a83efc142
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-15 00:19:23 +0530

    Added the ProductService parameter to the ProductController. Added pagination and sorting elements to the viewCategory() method. Also called the listEnabledByCategory() method in the viewCategory() method.

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/product/controller/ProductController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/product/controller/ProductController.java
@@ -2,25 +2,49 @@ package com.shopme.site.product.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.shopme.common.entity.Category;
+import com.shopme.common.entity.Product;
 import com.shopme.site.category.CategoryService;
+import com.shopme.site.exception.PageOutOfBoundsException;
+import com.shopme.site.product.ProductService;
 import com.shopme.site.product.exception.CategoryNotFoundException;
 
 @Controller
 public class ProductController {
 	private CategoryService categoryService;
+	private ProductService productService;
 
-	public ProductController(CategoryService categoryService) {
+	public ProductController(CategoryService categoryService, ProductService productService) {
 		this.categoryService = categoryService;
+		this.productService = productService;
 	}
 
 	@GetMapping("/category/{category_alias}")
 	public String viewCategory(@PathVariable("category_alias") String alias, Model model) {
+		int pageNumber = 1;
+
+		if (pageNumber <= 0) {
+			throw new PageOutOfBoundsException();
+		}
+
+		String sortField = "name";
+
+		if (sortField == null || sortField.isEmpty()) {
+			sortField = "name";
+		}
+
+		String sortDir = "asc";
+
+		if (sortDir == null || sortDir.isEmpty()) {
+			sortDir = "asc";
+		}
+
 		Category category = categoryService.getCategory(alias);
 
 		if (category == null) {
@@ -29,8 +53,34 @@ public class ProductController {
 
 		List<Category> parentsOfCategoryIncludingCategory = categoryService.getParentsOfCategoryIncludingCategory(category);
 
+		Page<Product> enabledProductsByCategory = productService.listEnabledByCategory(pageNumber, category.getId());
+
+		int totalPages = enabledProductsByCategory.getTotalPages();
+
+		if (pageNumber > totalPages) {
+			throw new PageOutOfBoundsException();
+		}
+
+		long startCount = (pageNumber - 1) * ProductService.PRODUCTS_PER_PAGE + 1;
+		long endCount = startCount + ProductService.PRODUCTS_PER_PAGE - 1;
+
+		long totalElements = enabledProductsByCategory.getTotalElements();
+
+		if (endCount > totalElements) {
+			endCount = totalElements;
+		}
+
+		model.addAttribute("pageNumber", pageNumber);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", (sortDir.equals("asc") ? "desc" : "asc"));
 		model.addAttribute("pageTitle", category.getName());
 		model.addAttribute("listCategoryAndParents", parentsOfCategoryIncludingCategory);
+		model.addAttribute("productsByCategory", enabledProductsByCategory);
+		model.addAttribute("totalPages", totalPages);
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", totalElements);
 
 		return "products_by_category";
 	}
