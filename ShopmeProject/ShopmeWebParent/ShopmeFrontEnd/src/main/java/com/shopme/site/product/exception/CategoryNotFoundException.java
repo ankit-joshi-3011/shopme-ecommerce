f660187ff106ff7cf11100f9a8d722ec226172b7
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-14 23:26:31 +0530

    Added a custom runtime exception for when a category was not found by its alias

---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/product/exception/CategoryNotFoundException.java
@@ -0,0 +1,9 @@
+package com.shopme.site.product.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@SuppressWarnings("serial")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The category you specified was not found")
+public class CategoryNotFoundException extends RuntimeException {
+}
