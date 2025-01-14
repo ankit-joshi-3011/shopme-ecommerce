84344c59fe448cb3116aa5cec5c93660fe695438
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-15 00:03:36 +0530

    Added the ProductRepository class with one method to list all enabled products by category

---
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/product/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.shopme.site.product;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.shopme.common.entity.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Integer>, PagingAndSortingRepository<Product, Integer> {
+	@Query("SELECT p FROM Product p WHERE p.enabled = true AND (p.category.id = ?1 OR p.category.allParentIds LIKE %?2%) ORDER BY p.name ASC")
+	public Page<Product> listEnabledByCategory(Integer categoryId, String categoryIdMatcher, Pageable pageable);
+}
