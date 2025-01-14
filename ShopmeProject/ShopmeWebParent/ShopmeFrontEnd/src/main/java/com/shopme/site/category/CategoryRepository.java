4842a114c54e9154f8b759ce8be6d1deb96d62d1
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-14 23:14:54 +0530

    Added a method to the category repository to find an enabled category with the provided alias

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/category/CategoryRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/category/CategoryRepository.java
@@ -10,4 +10,7 @@ import com.shopme.common.entity.Category;
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
 	@Query("SELECT c FROM Category c WHERE c.enabled = true ORDER BY c.name ASC")
 	public List<Category> findAllEnabled();
+
+	@Query("SELECT c FROM Category c WHERE c.enabled = true AND c.alias = ?1")
+	public Category findByAliasEnabled(String alias);
 }
