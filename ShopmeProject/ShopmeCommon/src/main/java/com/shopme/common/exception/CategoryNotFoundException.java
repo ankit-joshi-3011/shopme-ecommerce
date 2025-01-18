316f99d2847af6d24321d2e15ce9a1c6743a6ae8
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-19 00:55:34 +0530

    Moved the CategoryNotFoundException to the ShopmeCommon project

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/exception/CategoryNotFoundException.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/exception/CategoryNotFoundException.java
@@ -1,4 +1,4 @@
-package com.shopme.site.category.exception;
+package com.shopme.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
