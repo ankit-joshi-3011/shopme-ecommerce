78cd3b962487eafc46ee8526c65f87cb58dcd10a
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-01-21 02:04:08 +0530

    Made the ProductNotFoundException throw a 404 error if it is uncaught

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/exception/ProductNotFoundException.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/exception/ProductNotFoundException.java
@@ -1,6 +1,10 @@
 package com.shopme.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 @SuppressWarnings("serial")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The product you specified was not found")
 public class ProductNotFoundException extends RuntimeException {
 	public ProductNotFoundException(String message) {
 		super(message);
