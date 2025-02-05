0834f6d0cc9174dd45200411167a56e1ef408ef2
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-05 23:11:52 +0530

    Exposed the site-logo folder to the UI

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfigurer.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfigurer.java
@@ -11,8 +11,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfigurer implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String[] directoryNames = new String[] { "user-photos", "category-images", "brand-logo-images", "product-images" };
-		boolean[] parentDirectoryOrCurrentDirectory = new boolean[] { false, true, true, true };
+		String[] directoryNames = new String[] { "user-photos", "category-images", "brand-logo-images", "product-images", "site-logo" };
+		boolean[] parentDirectoryOrCurrentDirectory = new boolean[] { false, true, true, true, true };
 
 		for (int i = 0; i < directoryNames.length; i++) {
 			Path directoryPath = Paths.get((parentDirectoryOrCurrentDirectory[i] ? "../" : "") + directoryNames[i]);
