45946e994334eee1e76dc417b7917430e9088058
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-05 23:10:43 +0530

    Added the saveGeneralSettings() method which, for now, saves the site logo

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/controller/SettingController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/controller/SettingController.java
@@ -1,15 +1,25 @@
 package com.shopme.admin.setting.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.shopme.admin.FileUploadUtility;
 import com.shopme.admin.setting.CurrencyService;
 import com.shopme.admin.setting.SettingService;
 import com.shopme.common.entity.Currency;
 import com.shopme.common.entity.Setting;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class SettingController {
@@ -33,5 +43,25 @@ public class SettingController {
 		model.addAttribute("listCurrencies", listCurrencies);
 
 		return "settings/settings";
+	}
+
+	@PostMapping("/settings/save_general")
+	public String saveGeneralSettings(@RequestParam("fileImage") MultipartFile multipartFile, HttpServletRequest request, RedirectAttributes attributes) throws IOException {
+		if (!multipartFile.isEmpty()) {
+			final String siteLogoDirectory = "/site-logo/";
+
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+			String filePath = siteLogoDirectory + fileName;
+			settingService.getGeneralSettings().updateSiteLogo(filePath);
+
+			String uploadDirectory = ".." + siteLogoDirectory;
+			FileUploadUtility.cleanDirectory(uploadDirectory);
+			FileUploadUtility.saveFile(uploadDirectory, fileName, multipartFile);
+		}
+
+		attributes.addFlashAttribute("message", "General settings have been saved successfully");
+
+		return "redirect:/settings";
 	}
 }
