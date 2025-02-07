ed7a799715c882b24fef461b1d20238e448614ff
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-07 21:36:39 +0530

    Added the CountryRepositoryTests class

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/CountryRepositoryTests.java
@@ -0,0 +1,56 @@
+package com.shopme.admin.setting;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.State;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class CountryRepositoryTests {
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryRepositoryTests(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@Test
+	public void testCreateCountry() {
+		Country country = countryRepository.save(new Country("India", "IN", new HashSet<State>()));
+		assertThat(country).isNotNull();
+		assertThat(country.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testListCountries() {
+		List<Country> listCountries = countryRepository.findAllByOrderByNameAsc();
+		listCountries.forEach(country -> System.out.println(country.getName() + " " + country.getCode()));
+
+		assertThat(listCountries.size()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testUpdateCountry() {
+		Integer id = 1;
+		String name = "Republic of India";
+
+		Country country = countryRepository.findById(id).get();
+		country.setName(name);
+
+		Country updatedCountry = countryRepository.save(country);
+
+		assertThat(updatedCountry.getName()).isEqualTo(name);
+	}
+}
