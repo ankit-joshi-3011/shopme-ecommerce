d0fbb4e69d95429402f479a43d508633c3766430
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-07 21:26:01 +0530

    Added the Country and State entities

---
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
@@ -0,0 +1,69 @@
+package com.shopme.common.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "countries")
+public class Country {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, length = 45)
+	private String name;
+
+	@Column(nullable = false, length = 5)
+	private String code;
+
+	@OneToMany(mappedBy = "country")
+	private Set<State> states;
+
+	public Country() {
+	}
+
+	public Country(String name, String code, Set<State> states) {
+		this.name = name;
+		this.code = code;
+		this.states = states;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Set<State> getStates() {
+		return states;
+	}
+
+	public void setStates(Set<State> states) {
+		this.states = states;
+	}
+}
