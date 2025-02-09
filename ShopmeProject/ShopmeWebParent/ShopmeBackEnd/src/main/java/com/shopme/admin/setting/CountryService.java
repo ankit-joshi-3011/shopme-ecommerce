6bd2776dbadd66e09405254093cf36859019e56d
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-09 20:40:18 +0530

    Added a service method to delete a country by ID

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/CountryService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/CountryService.java
@@ -1,8 +1,11 @@
 package com.shopme.admin.setting;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.shopme.admin.setting.exception.CountryNotFoundException;
 import com.shopme.common.entity.Country;
 
 import jakarta.transaction.Transactional;
@@ -22,5 +25,15 @@ public class CountryService {
 
 	public Country save(Country country) {
 		return countryRepository.save(country);
+	}
+
+	public void delete(Integer id) throws CountryNotFoundException {
+		Optional<Country> country = countryRepository.findById(id);
+
+		if (country.isEmpty()) {
+			throw new CountryNotFoundException("Could not find any country with ID " + id);
+		}
+
+		countryRepository.deleteById(id);
 	}
 }
