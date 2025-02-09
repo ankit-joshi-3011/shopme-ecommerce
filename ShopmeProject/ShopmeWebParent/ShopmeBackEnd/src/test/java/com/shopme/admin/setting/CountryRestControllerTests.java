d1ab6ad836db3812c073a213ea3eec6abedf3177
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-09 20:15:43 +0530

    Added a test method to test the /countries/save endpoint

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/CountryRestControllerTests.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/CountryRestControllerTests.java
@@ -1,8 +1,12 @@
 package com.shopme.admin.setting;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashSet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,7 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shopme.common.entity.Country;
+import com.shopme.common.entity.State;
 
 // This is not a unit test but rather an integration test. This will run
 // the whole Spring Boot application.
@@ -49,5 +54,26 @@ public class CountryRestControllerTests {
 		for (Country country : countries) {
 			System.out.println(country.getName() + " " + country.getCode());
 		}
+	}
+
+	@Test
+	@WithMockUser(username = "nam@codejava.net", password = "nam2020", roles = "Admin")
+	public void testCreateCountry() throws Exception {
+		String url = "/countries/save";
+
+		String countryName = "Canada";
+		String countryCode = "CN";
+		Country canada = new Country(countryName, countryCode, new HashSet<State>());
+
+		MvcResult result = mockMvc.perform(post(url)
+			.contentType("application/json")
+			.content(objectMapper.writeValueAsString(canada))
+			.with(csrf()))
+			.andExpect(status().isOk())
+			.andDo(print())
+			.andReturn();
+
+		String jsonResponse = result.getResponse().getContentAsString();
+		System.out.println(jsonResponse);
 	}
 }
