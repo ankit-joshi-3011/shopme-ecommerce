06820813d0aeb0be4b67ed01e2aa24f4a5cd8321
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-09 20:08:04 +0530

    Added a controller method to save a country

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/controller/CountryRestController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/controller/CountryRestController.java
@@ -3,6 +3,8 @@ package com.shopme.admin.setting.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shopme.admin.setting.CountryService;
@@ -19,5 +21,12 @@ public class CountryRestController {
 	@GetMapping("/countries/list")
 	public List<Country> listAllCountries() {
 		return countryService.listAllCountries();
+	}
+
+	@PostMapping("/countries/save")
+	public String save(@RequestBody Country country) {
+		Country savedCountry = countryService.save(country);
+
+		return String.valueOf(savedCountry.getId());
 	}
 }
