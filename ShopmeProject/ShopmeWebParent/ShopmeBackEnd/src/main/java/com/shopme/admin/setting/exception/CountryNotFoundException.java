4c56022f4ffde1e74f99fed046d7a0c663f28814
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-09 20:37:38 +0530

    Added the CountryNotFoundException class

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/exception/CountryNotFoundException.java
@@ -0,0 +1,8 @@
+package com.shopme.admin.setting.exception;
+
+@SuppressWarnings("serial")
+public class CountryNotFoundException extends Exception {
+	public CountryNotFoundException(String message) {
+		super(message);
+	}
+}
