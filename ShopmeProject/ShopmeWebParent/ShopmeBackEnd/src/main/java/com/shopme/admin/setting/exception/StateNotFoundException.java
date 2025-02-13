90ffa87bb15c128194f2945360a9a32bf3e97971
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-13 19:17:58 +0530

    Added the StateNotFoundException class

---
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/exception/StateNotFoundException.java
@@ -0,0 +1,8 @@
+package com.shopme.admin.setting.exception;
+
+@SuppressWarnings("serial")
+public class StateNotFoundException extends Exception {
+	public StateNotFoundException(String message) {
+		super(message);
+	}
+}
