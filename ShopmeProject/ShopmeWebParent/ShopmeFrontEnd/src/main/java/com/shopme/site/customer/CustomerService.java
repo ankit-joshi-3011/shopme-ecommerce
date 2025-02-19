319ba03b8f14a101e936473ab3c77feb7b4e1e18
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-19 16:01:37 +0530

    Added a service method to check whether the email address that the customer entered is unique or not

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/customer/CustomerService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/customer/CustomerService.java
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.shopme.common.dto.CountryDTO;
 import com.shopme.common.entity.Country;
+import com.shopme.common.entity.Customer;
 import com.shopme.site.setting.CountryRepository;
 
 @Service
 public class CustomerService {
 	private CountryRepository countryRepository;
+	private CustomerRepository customerRepository;
 
-	public CustomerService(CountryRepository countryRepository) {
+	public CustomerService(CountryRepository countryRepository, CustomerRepository customerRepository) {
 		this.countryRepository = countryRepository;
+		this.customerRepository = customerRepository;
 	}
 
 	public List<CountryDTO> listAllCountries() {
@@ -27,5 +30,11 @@ public class CustomerService {
 		}
 
 		return countryDtos;
+	}
+
+	public boolean isEmailUnique(String email) {
+		Customer customer = customerRepository.findByEmail(email);
+
+		return customer == null;
 	}
 }
