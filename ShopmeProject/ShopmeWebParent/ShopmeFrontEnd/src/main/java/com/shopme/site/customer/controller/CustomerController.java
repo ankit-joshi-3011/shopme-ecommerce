019be84f8636bd138e07ba4be804591458425950
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-02-25 02:59:31 +0530

    Added the createCustomer() method in the CustomerController

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/customer/controller/CustomerController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/customer/controller/CustomerController.java
@@ -1,21 +1,34 @@
 package com.shopme.site.customer.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shopme.common.dto.CountryDTO;
 import com.shopme.common.entity.Customer;
 import com.shopme.site.customer.CustomerService;
+import com.shopme.site.setting.EmailSettingsBag;
+import com.shopme.site.setting.SettingService;
+import com.shopme.site.utility.Utility;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class CustomerController {
 	private CustomerService customerService;
+	private SettingService settingService;
 
-	public CustomerController(CustomerService customerService) {
+	public CustomerController(CustomerService customerService, SettingService settingService) {
 		this.customerService = customerService;
+		this.settingService = settingService;
 	}
 
 	@GetMapping("/register")
@@ -26,5 +39,34 @@ public class CustomerController {
 		model.addAttribute("customer", new Customer());
 
 		return "register/registration_form";
+	}
+
+	@PostMapping("/create_customer")
+	public String createCustomer(Customer customer, HttpServletRequest request) throws UnsupportedEncodingException, MessagingException {
+		customerService.registerCustomer(customer);
+
+		EmailSettingsBag emailSettingsBag = settingService.getEmailSettingsBag();
+		JavaMailSenderImpl mailSenderImpl = Utility.prepareMailSender(emailSettingsBag);
+
+		String toAddress = customer.getEmail();
+		String subject = emailSettingsBag.getCustomerVerifySubject();
+
+		String content = emailSettingsBag.getCustomerVerifyContent();
+		content = content.replace("[[name]]", customer.getFullName());
+
+		String verifyUrl = Utility.getSiteURL(request) + "/verify?code=" + customer.getVerificationCode();
+		content = content.replace("[[URL]]", verifyUrl);
+
+		MimeMessage message = mailSenderImpl.createMimeMessage();
+		MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+
+		messageHelper.setFrom(emailSettingsBag.getFromAddress(), emailSettingsBag.getSenderName());
+		messageHelper.setTo(toAddress);
+		messageHelper.setSubject(subject);
+		messageHelper.setText(content, true);
+
+		mailSenderImpl.send(message);
+
+		return "register/register_success";
 	}
 }
