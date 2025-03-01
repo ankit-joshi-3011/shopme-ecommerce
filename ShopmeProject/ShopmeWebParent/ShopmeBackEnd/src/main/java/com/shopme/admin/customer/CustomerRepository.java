91900d3d82764da95c2909f7df27596cf52fe301
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-01 14:56:46 +0530

    Added a method to find all customers with the specified keyword

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerRepository.java
@@ -1,9 +1,14 @@
 package com.shopme.admin.customer;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.shopme.common.entity.Customer;
 
 public interface CustomerRepository extends CrudRepository<Customer, Integer>, PagingAndSortingRepository<Customer, Integer> {
+	@Query("SELECT c FROM Customer c WHERE CONCAT(c.id, ' ', c.email, ' ', c.firstName, ' ', c.lastName, ' ', c.city, ' ', c.state, ' ', c.country.name) LIKE %?1%")
+	public Page<Customer> findAll(String keyword, Pageable pageable);
 }
