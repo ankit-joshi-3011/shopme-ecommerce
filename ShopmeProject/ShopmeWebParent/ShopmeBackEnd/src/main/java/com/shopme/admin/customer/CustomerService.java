0e771abc540f767cac352c799ea21b5efd06eb1b
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-01 14:57:11 +0530

    Added a method to list customers by page

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerService.java
@@ -3,12 +3,18 @@ package com.shopme.admin.customer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Customer;
 
 @Service
 public class CustomerService {
+	public static final int CUSTOMERS_PER_PAGE = 10;
+
 	private CustomerRepository customerRepository;
 
 	public CustomerService(CustomerRepository customerRepository) {
@@ -25,5 +31,18 @@ public class CustomerService {
 		}
 
 		return returnedCustomers;
+	}
+
+	public Page<Customer> listByPage(int pageNumber, String sortField, String sortDirection, String keyword) {
+		Sort sort = Sort.by(sortField);
+		sort = sortDirection.equals("asc") ? sort.ascending() : sort.descending();
+
+		Pageable pageable = PageRequest.of(pageNumber - 1, CUSTOMERS_PER_PAGE, sort);
+
+		if (keyword != null) {
+			return customerRepository.findAll(keyword, pageable);
+		}
+
+		return customerRepository.findAll(pageable);
 	}
 }
