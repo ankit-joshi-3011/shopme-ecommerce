2e417d36af8f968223e4c4fbb5886b0c10666dd1
Ankit Joshi <ankit.joshi2406@gmail.com>
2025-03-03 17:35:16 +0530

    Added the updateEnabledStatus method to the CustomerRepository

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.shopme.admin.customer;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -11,4 +12,8 @@ import com.shopme.common.entity.Customer;
 public interface CustomerRepository extends CrudRepository<Customer, Integer>, PagingAndSortingRepository<Customer, Integer> {
 	@Query("SELECT c FROM Customer c WHERE CONCAT(c.id, ' ', c.email, ' ', c.firstName, ' ', c.lastName, ' ', c.city, ' ', c.state, ' ', c.country.name) LIKE %?1%")
 	public Page<Customer> findAll(String keyword, Pageable pageable);
+
+	@Query("UPDATE Customer c SET c.enabled = ?2 WHERE c.id = ?1")
+	@Modifying
+	public void updateEnabledStatus(Integer id, boolean enabledOrDisabled);
 }
